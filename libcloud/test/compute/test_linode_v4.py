37f976b2d98bfeb6bfc5e45273d2ce6b3a8b3a78
dimgal1 <dimgal20@gmail.com>
2020-10-26 16:11:39 +0200

    Fix StorageVolume id and reverse params in attach_volume
    - Cast StorageVolume id to str
    - Change params in attach_volume to be in the same order as base class

--- libcloud/test/compute/test_linode_v4.py
+++ libcloud/test/compute/test_linode_v4.py
@@ -279,7 +279,7 @@ class LinodeTestsV4(unittest.TestCase, TestCaseMixin):
         self.assertEqual(len(volumes), 2)
         volume = volumes[0]
         volume_2 = volumes[1]
-        self.assertEqual(volume.id, 12345)
+        self.assertEqual(volume.id, '12345')
         self.assertEqual(volume.name, 'Testvolume1')
         self.assertEqual(volume.size, 100)
         self.assertEqual(volume.extra['linode_id'], 456353688)
