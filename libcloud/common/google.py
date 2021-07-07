cb1be3b0bc2b60b3b2812e46221f4c4ba6124378
Christos Karageorgiou Kaneen <csk@mist.io>
2021-07-07 16:31:23 +0000

    Replace GoogleBaseConnection superclass
    Needed redirect_uri parameter

--- libcloud/common/google.py
+++ libcloud/common/google.py
@@ -731,7 +731,7 @@ class GoogleOAuth2Credential(object):
                      filename, str(e))
 
 
-class GoogleBaseConnection(ConnectionUserAndKey, PollingConnection):
+class GoogleBaseConnection(GoogleBaseAuthConnection, PollingConnection):
     """Base connection class for interacting with Google APIs."""
 
     driver = GoogleBaseDriver
