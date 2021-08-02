d42631799958373a90f38c5f0ebbe764676a3a6d
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-02 20:25:59 +0300

    Correct import

--- libcloud/container/drivers/openshift.py
+++ libcloud/container/drivers/openshift.py
@@ -1,5 +1,5 @@
 from libcloud.common.openshift import OpenShiftBasicAuthConnection
-from libcloud.container.kubernetes import KubernetesContainerDriver
+from libcloud.container.drivers.kubernetes import KubernetesContainerDriver
 
 
 class OpenShiftContainerDriver(KubernetesContainerDriver):
